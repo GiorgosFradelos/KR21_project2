0708e0d591498deed363b594c048c669833e516a
Yorgos <gi.fra1997@gmail.com>
2022-12-06 23:17:47 +0100

    marginal distribution, factor multiplication, summing out (wrong results)

--- BNReasoner.py
+++ BNReasoner.py
@@ -1,4 +1,5 @@
 import copy
+import random
 from typing import Union
 import pandas as pd
 from BayesNet import BayesNet
@@ -90,8 +91,288 @@ class BNReasoner:
     def check_independence(self):
         return True
 
-    def marginalization(self):
-        return True
+
+
+
+    def randomOrder(self, X: list) -> list:
+        random.shuffle(X)
+        return X
+
+    def summing_out(self, CPT, index_same, list):
+        # create the final new CPT without the variables that should be summed out
+        new_CPT = CPT.copy()
+        for variable in list:
+            new_CPT = new_CPT.drop(columns=[variable])
+
+        # loop through the keys from the dictionary
+        for key in index_same:
+
+            # pick the p_value from that key
+            p_value_sum = CPT.iloc[key]['p']
+
+            # pick the values from the key, so the equal indexes
+            equal_indexes = [index_same.get(key)]
+
+            # loop through the equal indexes
+            for i in equal_indexes:
+                # add this p-value from this row to the total p-val
+                p = CPT.iloc[i]['p'].values
+                p_value_sum += p[0]
+
+                # set this value to zero to be able to delete it later
+                new_CPT.at[i[0], 'p'] = 0
+
+            # set the new_CPT key value to the new p value
+
+            new_CPT.at[key, 'p'] = p_value_sum
+
+        for index_CPT in range(len(new_CPT)):
+            if new_CPT.iloc[index_CPT]['p'] == 0:
+                new_CPT.drop([index_CPT])
+
+        return
+
+    def check_double(self, CPT, list, type):
+        """
+        function to sum all the values from a list out
+        CPT: pandas dataframe
+        list: list
+        returns: pandas dataframe
+        """
+
+        # create a dictionary for the equal rows with same values
+        index_same = {}
+
+        # create a CPT without p-values and without all the variables that should be summed out
+        clean_CPT = CPT.copy()
+
+        clean_CPT = clean_CPT.drop(columns=["p"])
+        for variable in list:
+            clean_CPT = clean_CPT.drop(columns=[variable])
+
+        # loop trough the length of rows of the clean CPT
+        for row_1 in clean_CPT.iloc:
+            for i in range(row_1.name + 1, len(clean_CPT)):
+                row_2 = clean_CPT.iloc[i]
+
+                # compare the different rows
+                if row_1.equals(row_2):
+
+                    # if it is still empty just add the new key with index number and index of equal row as value
+                    if row_1.name in index_same:
+                        index_same[row_1.name].append(i)
+                    else:
+                        index_same[row_1.name] = [i]
+
+        if type == "sum":
+            new_CPT = self.summing_out(CPT, index_same, list)
+        elif type == "max":
+            new_CPT = self.maxing_out(CPT, index_same)
+
+        return new_CPT
+
+    def multiplying_factors(self, CPT_1, CPT_2):
+        CPT_1 = CPT_1.reset_index(drop=True)
+        CPT_2 = CPT_2.reset_index(drop=True)
+
+        # matching columns
+        columns_1 = list(CPT_1)
+        columns_2 = list(CPT_2)
+        columns = [x for x in columns_1 if x in columns_2]
+
+        # create a dictionary for the equal rows with same values
+        index_same = {}
+
+        # create a CPT without p-values and without all the variables that should be summed out
+        clean_CPT_1 = CPT_1.copy()
+        clean_CPT_2 = CPT_2.copy()
+
+        clean_CPT_1 = clean_CPT_1[columns]
+        clean_CPT_1 = clean_CPT_1.drop(columns="p")
+
+        clean_CPT_2 = clean_CPT_2[columns]
+        clean_CPT_2 = clean_CPT_2.drop(columns="p")
+
+        # loop trough the length of rows of the clean CPT
+        for row_1 in clean_CPT_1.iloc:
+
+            row_1 = row_1.replace([True], 1.0)
+            row_1 = row_1.replace([False], 0.0)
+
+            for row_2 in clean_CPT_2.iloc:
+                row_2 = row_2.replace([True], 1.0)
+                row_2 = row_2.replace([False], 0.0)
+
+                # compare the different rows
+                if row_1.equals(row_2):
+
+                    # if it is still empty just add the new key with index number and index of equal row as value
+                    if row_1.name in index_same:
+                        index_same[row_1.name].append(row_2.name)
+                    else:
+                        index_same[row_1.name] = [row_2.name]
+
+        new_columns = columns_1.copy()
+        new_columns.remove('p')
+        new_columns.extend(x for x in columns_2 if x not in new_columns)
+
+        new_CPT = pd.DataFrame()
+        merge_CPT_1 = CPT_1.copy()
+
+        merge_CPT_2 = CPT_2.copy()
+
+        for key, values in index_same.items():
+
+            # merge rows
+            row_1 = merge_CPT_1.iloc[key].drop("p")
+            for value in values:
+                # merge_CPT_2.reset_index()
+                row_2 = merge_CPT_2.iloc[value].drop("p")
+
+                difference = [name for name in new_columns if name not in columns_1]
+
+                new_row = pd.merge(row_1, row_2[difference], left_index=True, right_index=True, how='outer')
+                new_row = new_row.iloc[:, 0].fillna(new_row.iloc[:, 1])
+                p_1 = CPT_1.iloc[key]["p"]
+                p_2 = CPT_2.iloc[value]["p"]
+                new_p = p_1 * p_2
+
+                new_row["p"] = round(new_p, 8)
+
+                new_CPT = new_CPT.append(new_row, ignore_index=True)
+
+        return new_CPT
+
+    def multiply_cpts(self, cpt1, cpt2):
+        new_CPT = pd.DataFrame()
+        columns2 = list(cpt2)
+        columns1 = list(cpt1)
+        # diff = [x for x in columns1 if x in columns2]
+        # print(f'{diff} DIFFF')
+        for i in range(len(cpt1)):
+            for j in range(len(cpt2)):
+                p_1 = cpt1.iloc[i]["p"]
+                p_2 = cpt2.iloc[j]["p"]
+
+                new_p = round(p_1 * p_2, 8)
+
+                clean = cpt1.iloc[i].drop(['p'])
+
+                new_row = clean.append(cpt2.iloc[j])
+
+                new_row["p"] = new_p
+
+                new_CPT = new_CPT.append(new_row, ignore_index=True)
+        final_cpt = new_CPT[new_CPT['p'] != 0]
+
+        return final_cpt
+
+    def marginal_distribution(self, Q, evidence, order):
+        '''
+        Q = variables in the network BN
+        evidence = instantiation of some variables in the BN
+        output = posterior marginal Pr(Q|e)
+        '''
+
+        self.prune(Q, evidence)
+        not_Q = [x for x in self.bn.get_all_variables() if x not in Q]
+        if order == 1:
+            order = set(self.minDegreeOrder(not_Q))
+        elif order == 2:
+            order = set(self.minFillOrder(not_Q))
+        elif order == 3:
+            order = set(self.randomOrder(not_Q))
+
+        # order = set(self.randomOrder(self.bn.get_all_variables()))
+        # order_no_Q = order.difference(Q)
+
+        # get all cpts eliminating rows incompatible with evidence
+        for ev in evidence.keys():
+
+            cpts = self.bn.get_cpt(ev)
+            cpts = self.bn.reduce_factor(evidence, cpts)
+
+            for row in range(len(cpts)):
+                if cpts.iloc[row]['p'] == 0:
+                    cpts.drop([row])
+
+            print(f'\ncpts: {cpts}')
+
+            self.bn.update_cpt(ev, cpts)
+
+            for child in self.bn.get_children(ev):
+                cpts = self.bn.get_cpt(child)
+                cpts = self.bn.reduce_factor(evidence, cpts)
+
+                for row in range(len(cpts)):
+                    if cpts.iloc[row]['p'] == 0:
+                        cpts.drop([row])
+
+                self.bn.update_cpt(child, cpts)
+
+        # make CPTs of all variables in Pi not in Q
+        S = list(self.bn.get_all_cpts().values())
+
+        for variable in order:
+            list_cpts = []
+            list_goed = []
+            print(f'\nVariable: {variable}')
+            for i in range(0, len(S)-1):
+
+                print(f'\n\nS[{i}]:\n{S[i]}')
+
+                if S[i] is not None:
+                    columns = list(S[i])
+                    if variable in columns:
+                        list_cpts.append(S[i])
+                    else:
+                        list_goed.append(S[i])
+
+            if len(list_cpts) > 0:
+                cpt1 = list_cpts[0]
+
+            if len(list_cpts) == 1:
+                list_goed.append(list_cpts[0])
+
+            if len(list_cpts) > 1:
+                for cpt2 in list_cpts[1:]:
+                    cpt1 = self.multiplying_factors(cpt1, cpt2)
+                final_cpt = cpt1
+
+                print(f'\ncpt1:\n{cpt1}')
+
+                factor = self.check_double(final_cpt, [variable], 'sum')
+                list_goed.append(factor)
+
+            S = list_goed
+
+        for i in range(0, len(S) - 1):
+            if len(set(list(S[i])).intersection(set(list(S[i])))) > 1:
+                cpt_new = self.multiplying_factors(S[i], S[i + 1])
+            else:
+                cpt_new = self.multiply_cpts(S[i], S[i + 1])
+            S[i + 1] = cpt_new
+
+        final_cpt = cpt_new
+        final_cpt = final_cpt[final_cpt['p'] != 0]
+
+        for var in list(cpt_new):
+            if var != "p":
+                if var not in Q:
+                    final_cpt = self.check_double(final_cpt, [var], 'sum')
+
+        normalize_factor = final_cpt['p'].sum()
+        final_cpt['p'] = final_cpt['p'] / normalize_factor
+
+        # zero
+        final_cpt = final_cpt[final_cpt['p'] != 0]
+
+        return final_cpt
+
+
+
+
+
 
     def maxing_out(self):
         return True
--- main.py
+++ main.py
@@ -6,7 +6,8 @@ import pandas as pd
 
 ''' Group 5 - Start '''
 #filename = 'testing/dog_problem.BIFXML'
-filename = 'testing/lecture_example.BIFXML'
+filename = 'testing/alarm.BIFXML'
+#filename = 'testing/lecture_example.BIFXML'
 #filename = 'testing/lecture_example2.BIFXML'
 
 
@@ -14,53 +15,44 @@ filename = 'testing/lecture_example.BIFXML'
 new_BN = reas.BNReasoner(filename)
 #new_BN.bn.draw_structure()
 
-edges = new_BN.bn.edges
+#edges = new_BN.bn.edges
 
 
 ''' Use Bayesian Network class functions to test their functionality '''
 ''' NOT NEEDED ANYMORE '''
-#x_y = [['Winter?'], ['Rain?']]
-#x_y = [['Sprinkler?'], ['Wet Grass?']]
-x_y = [['Winter?'], ['Slippery Road?']]
 
 
-d = {'Winter?': True, 'Slippery Road?': True}
-#d = {'Sprinkler?': True, 'Wet Grass?': True}
 
-#ev = pd.Series(data=d, index=['Winter?', 'Rain?'])
-ev = pd.Series(data=d, index=['Rain?'])
-
-#query_vars = {"Wet Grass?"}
-#query_vars = {"Sprinkler?", "Wet Grass?"}
-query_vars = {"Winter?", "Slippery Road?"}
 
 
 #new_BN.prune(query_vars, ev)
 #new_BN.bn.draw_structure()
 
-print(new_BN.bn.get_cpt("Sprinkler?"))
-print("------------")
-print(new_BN.bn.get_cpt("Rain?"))
-print("------------")
-print(new_BN.bn.get_cpt("Wet Grass?"))
+query_vars = ['B']
+evidence= {'J': True, 'M': False}
+
+index = list(evidence.keys())
+ev = pd.Series(data=evidence, index=index)
 
 
 
 
 
-#e=['Slippery Road?']
-e=['Rain?']
 
-print(f'\n{x_y[0]} _|_ {x_y[1]}\t|\t{e} : {new_BN.d_sep(x_y[0], x_y[1], e)}')
 
-new_BN.prune(query_vars, ev)
-new_BN.bn.draw_structure()
+
+
+#print(f'\n{x_y[0]} _|_ {x_y[1]}\t|\t{e} : {new_BN.d_sep(x_y[0], x_y[1], e)}')
+
+#new_BN.prune(query_vars, ev)
+#new_BN.bn.draw_structure()
 
 cpts = new_BN.bn.get_all_cpts()
 variables = new_BN.bn.get_all_variables()
 #print(f'{variables}')
 
-
+order = 3
+final_cpt = new_BN.marginal_distribution(query_vars, ev, order)
 
 '''vars = []
 for var in variables:
